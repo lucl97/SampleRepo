04695a384172f4780a08dee5b98302641725a753
lucl97 <luc.lallement@colostate.edu>
2022-10-14 13:32:52 -0600

    Added to readme & created new code file.

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ AREC 570 in class example
 This is a change
 
 New change to read me 10_14_2022
+
+Experimental code blah blah blah.
---
+++ code/02_cars_sample_lm.R
@@ -0,0 +1,4 @@
+#Here is a new script to make a point about differences across branches.
+
+model <- lm(mtcars$mpg ~ mtcars$cyl)
+summary(model)
